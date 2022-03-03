fb642118f75c097927149c1f304cc967310bd05e
Pandrapragada Himabindhu <Pandrapragada.Himabindhu@ibm.com>
2022-03-03 15:22:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<int, string> dic = new Dictionary<int, string>();
+            //dic.Add(11, "himabindhu");
+            //dic.Add(15, "varsha");
+            //foreach (int k in dic.Keys)
+            //{
+            //    Console.WriteLine("{0}", k);
+            //}
+            //foreach(string val in dic.Values)
+            //{
+            //    Console.WriteLine("{0}", val);
+            //}
+            //Console.WriteLine();
+            //Console.readline();
+            //program on stacks
+            //Stack ts = new Stack ( );
+            // ts.Push(1);
+            // ts.Push(2);
+            // ts.Push(3);
+            // ts.Push("my name is himabindhu");
+            // foreach (var Stackitem in ts)
+            // {
+            //     Console.WriteLine(Stackitem);
+            // }
+            //     Console.ReadLine();
+            //program on queue
+            //Queue myQueue = new Queue();
+            //myQueue.Enqueue(23);
+            //myQueue.Enqueue(56);
+            //myQueue.Enqueue("IBM");
+            //foreach ( var elements in myQueue)
+            //{
+            //    Console.WriteLine(elements);
+            //}
+            //Console.ReadLine();
+            //program on hashtable
+            //Hashtable myHashtable = new Hashtable();
+            //myHashtable.Add(1, "one");
+            //myHashtable.Add(2, "two");
+            //myHashtable.Add(4, "four");
+            //foreach (DictionaryEntry de in myHashtable)
+            //{
+            //    Console.WriteLine("Key: {0}, Value: {1}", de.Key, de.Value);
+            //}
+            //Console.ReadLine();
+            //program on null
+            //Nullable<bool> i = new Nullable<bool>();
+            //int? x = null;
+            //if (x == null)
+            //{
+
+            //    Console.WriteLine("true");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("flase");
+            //}
+            //Console.ReadLine();
+            //program on coalescing null
+            string name = "bindhu";
+            string myname = name ?? "Laxmi";
+            Console.WriteLine(myname);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
